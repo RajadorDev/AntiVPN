e852abf001217dbd42428836caa35e2749da7187
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2024-05-03 14:32:14 -0300

    update src/AntiVPN/event/StartCheckEvent.php

---
+++ src/AntiVPN/event/StartCheckEvent.php
@@ -0,0 +1,27 @@
+<?php
+
+declare (strict_types = 1);
+
+/*
+  
+  Rajador Developer
+
+  ▒█▀▀█ ░█▀▀█ ░░░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀█ 
+  ▒█▄▄▀ ▒█▄▄█ ░▄░▒█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▀ 
+  ▒█░▒█ ▒█░▒█ ▒█▄▄█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█░▒█
+
+  GitHub: https://github.com/RajadorDev
+
+  Discord: rajadortv
+
+
+*/
+
+namespace AntiVPN\event;
+
+use pocketmine\event\Cancellable;
+
+final class StartCheckEvent extends AntiVPNEvent implements Cancellable 
+{}
+
+?>
