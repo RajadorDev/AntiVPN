c6836720458987252694c8b0adacc429b0de234c
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2024-05-03 13:19:52 -0300

    update src/AntiVPN/utils/AntiVPNAPI.php

---
+++ src/AntiVPN/utils/AntiVPNAPI.php
@@ -0,0 +1,32 @@
+<?php
+
+declare (strict_types = 1);
+
+/*
+  
+  Rajador Developer
+
+  ▒█▀▀█ ░█▀▀█ ░░░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀█ 
+  ▒█▄▄▀ ▒█▄▄█ ░▄░▒█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▀ 
+  ▒█░▒█ ▒█░▒█ ▒█▄▄█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█░▒█
+
+  GitHub: https://github.com/RajadorDev
+
+  Discord: rajadortv
+
+
+*/
+
+namespace AntiVPN\utils;
+
+final class AntiVPNAPI 
+{
+	
+	public static function isValidKey(String $key) : bool 
+	{
+		return strlen(trim($key)) > 1 && strpos($key, ' ') === false;
+	}
+	
+}
+
+?>
