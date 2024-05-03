54324b2deb04fccb584c203a167ee2cb1dc31454
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2024-05-03 13:47:15 -0300

    update src/AntiVPN/event/AntiVPNEvent.php

---
+++ src/AntiVPN/event/AntiVPNEvent.php
@@ -0,0 +1,46 @@
+<?php
+
+declare (strict_types = 1);
+
+/*
+  
+  Rajador Developer
+
+  ▒█▀▀█ ░█▀▀█ ░░░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀█ 
+  ▒█▄▄▀ ▒█▄▄█ ░▄░▒█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▀ 
+  ▒█░▒█ ▒█░▒█ ▒█▄▄█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█░▒█
+
+  GitHub: https://github.com/RajadorDev
+
+  Discord: rajadortv
+
+
+*/
+
+namespace AntiVPN\event;
+
+use AntiVPN\Manager;
+
+use pocketmine\player\Player;
+
+use pocketmine\event\player\PlayerEvent;
+
+abstract class AntiVPNEvent extends PlayerEvent 
+{
+	
+	/**
+	 * @param Player $player
+ 	**/
+	public function __construct(Player $player) 
+	{
+		$this->player = $player;
+	}
+	
+	public function getManager() : Manager 
+	{
+		return Manager::getInstance();
+	}
+	
+}
+
+?>
