38307222e148f1c926bc67efb8c65eddabe9dcce
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2024-05-04 10:01:44 -0300

    update src/AntiVPN/event/FinishCheckEvent.php

---
+++ src/AntiVPN/event/FinishCheckEvent.php
@@ -0,0 +1,41 @@
+<?php
+
+declare (strict_types = 1);
+
+/*
+  
+  Rajador Developer
+
+  ▒█▀▀█ ░█▀▀█ ░░░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀█ 
+  ▒█▄▄▀ ▒█▄▄█ ░▄░▒█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▀ 
+  ▒█░▒█ ▒█░▒█ ▒█▄▄█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█░▒█
+
+  GitHub: https://github.com/RajadorDev
+
+  Discord: rajadortv
+
+
+*/
+
+namespace AntiVPN\event;
+
+use pocketmine\event\Event;
+
+class FinishCheckEvent extends Event 
+{
+	
+	public function __construct(protected String $ip, protected String $username) {}
+	
+	public function getUsername() : String 
+	{
+		return $this->username;
+	}
+	
+	public function getIp() : String 
+	{
+		return $this->ip;
+	}
+	
+}
+
+?>
