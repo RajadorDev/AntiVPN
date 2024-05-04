8a34eac26b73fb34b98809fb65af02a4d7099c6e
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2024-05-04 11:09:51 -0300

    update src/AntiVPN/form/ConfirmForm.php

--- src/AntiVPN/form/ConfirmForm.php
+++ src/AntiVPN/form/ConfirmForm.php
@@ -21,13 +21,30 @@ namespace AntiVPN\form;
 
 use AntiVPN\libs\form\ModalForm;
 
+use pocketmine\player\Player;
+
 final class ConfirmForm extends ModalForm 
 {
 	
 	public function __construct(String $title, String $description, String $buttonConfirmText, String $buttonDeclineText) 
 	{
-		
+		parent::__construct 
+		(
+			function (Player $player, bool $value) : void 
+			{
+				if ($value)
+				{
+					$this->onConfirm($player);
+				}
+			}
+		);
+		$this->setTitle($title);
+		$this->setContent($description);
+		$this->setButton1($buttonConfirmText);
+		$this->setButton2($buttonDeclineText);
 	}
+	
+	abstract public function onConfirm(Player $player) : void;
 	
 }
 
