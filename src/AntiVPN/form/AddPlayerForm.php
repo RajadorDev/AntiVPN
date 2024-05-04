284ca2982ab558f05017b75f86675a3e9552aae4
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2024-05-04 10:57:59 -0300

    update src/AntiVPN/form/AddPlayerForm.php

---
+++ src/AntiVPN/form/AddPlayerForm.php
@@ -0,0 +1,47 @@
+<?php
+
+declare (strict_types = 1);
+
+/*
+  
+  Rajador Developer
+
+  ▒█▀▀█ ░█▀▀█ ░░░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀█ 
+  ▒█▄▄▀ ▒█▄▄█ ░▄░▒█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▀ 
+  ▒█░▒█ ▒█░▒█ ▒█▄▄█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█░▒█
+
+  GitHub: https://github.com/RajadorDev
+
+  Discord: rajadortv
+
+
+*/
+
+namespace AntiVPN\form;
+
+use AntiVPN\libs\form\SimpleForm;
+
+use pocketmine\player\Player;
+
+class AddPlayerForm extends SimpleForm 
+{
+	
+	const TARGET = 'player_name';
+	
+	public function __construct(Player $sender) 
+	{
+		parent::__construct 
+		(
+			function (Player $player, mixed $data) : void 
+			{
+				
+			}
+		);
+		$this->setTitle('§aAdd Player in §6Anti§cVPN§f Whitelist');
+		$this->addLabel('§7When added, the AntiVPN system will ignore the player.');
+		$this->addInput('Player name:', 'Exemple: ' . $sender->getName(), '', self::TARGET);
+	}
+	
+}
+
+?>
