ffc2708e7abe9d52c6a34a8f47a78c977338852a
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2024-05-04 11:04:06 -0300

    update src/AntiVPN/form/ConfirmForm.php

---
+++ src/AntiVPN/form/ConfirmForm.php
@@ -0,0 +1,34 @@
+<?php
+
+declare (strict_types = 1);
+
+/*
+  
+  Rajador Developer
+
+  ▒█▀▀█ ░█▀▀█ ░░░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀█ 
+  ▒█▄▄▀ ▒█▄▄█ ░▄░▒█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▀ 
+  ▒█░▒█ ▒█░▒█ ▒█▄▄█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█░▒█
+
+  GitHub: https://github.com/RajadorDev
+
+  Discord: rajadortv
+
+
+*/
+
+namespace AntiVPN\form;
+
+use AntiVPN\libs\form\ModalForm;
+
+final class ConfirmForm extends ModalForm 
+{
+	
+	public function __construct(String $title, String $description, String $buttonConfirmText, String $buttonDeclineText) 
+	{
+		
+	}
+	
+}
+
+?>
