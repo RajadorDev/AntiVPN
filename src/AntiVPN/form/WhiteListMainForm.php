03b8e52ff5078f3cee4b67183f1d1a7ee5f4233d
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2024-05-04 10:45:52 -0300

    update src/AntiVPN/form/WhiteListMainForm.php

---
+++ src/AntiVPN/form/WhiteListMainForm.php
@@ -0,0 +1,71 @@
+<?php
+
+declare (strict_types = 1);
+
+/*
+  
+  Rajador Developer
+
+  ▒█▀▀█ ░█▀▀█ ░░░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀█ 
+  ▒█▄▄▀ ▒█▄▄█ ░▄░▒█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▀ 
+  ▒█░▒█ ▒█░▒█ ▒█▄▄█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█░▒█
+
+  GitHub: https://github.com/RajadorDev
+
+  Discord: rajadortv
+
+
+*/
+
+namespace AntiVPN\form;
+
+use AntiVPN\libs\form\SimpleForm;
+
+use pocketmine\player\Player;
+
+class WhiteListMainForm extends SimpleForm 
+{
+	
+	const ADD = 'add_player';
+	
+	const REMOVE = 'remove_player';
+	
+	const LIST = 'see_list';
+	
+	public function __construct() 
+	{
+		parent::__construct 
+		(
+			function (Player $player, mixed $data) : void 
+			{
+				switch ($data)
+				{
+					case self::ADD:
+						
+					break;
+					case self::REMOVE:
+						
+					break;
+					case self::LIST:
+						
+					break;
+				}
+			}
+		);
+		$this->setTitle('§6Anti§cVPN §fWhiteList');
+		foreach 
+		(
+			[
+				self::ADD => ['§aAdd Player', 'textures/gui/newgui/Friends.png'],
+				self::REMOVE => ['§cRemove Player', 'textures/gui/newgui/X.png'],
+				self::LIST => ['§eList', 'textures/gui/newgui/Realms.png']
+			] as $id => $data
+		)
+		{
+			$this->addButton($data[0], self::IMAGE_TYPE_PATH, $data[1], $id);
+		}
+	}
+	
+}
+
+?>
