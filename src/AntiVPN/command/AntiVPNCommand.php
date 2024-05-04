dd7629e2da5771909a722939478913946542cc1c
RajadorDev <125204182+RajadorDev@users.noreply.github.com>
2024-05-04 13:15:37 -0300

    update src/AntiVPN/command/AntiVPNCommand.php

--- src/AntiVPN/command/AntiVPNCommand.php
+++ src/AntiVPN/command/AntiVPNCommand.php
@@ -23,6 +23,10 @@ use AntiVPN\Manager;
 
 use AntiVPN\utils\AntiVPNAPI;
 
+use AntiVPN\form\{WhiteListMainForm, ConfirmAddWhiteListForm, RemovePlayerForm};
+
+use pocketmine\player\Player;
+
 use pocketmine\command\{Command, CommandSender};
 
 final class AntiVPNCommand extends Command 
@@ -33,6 +37,9 @@ final class AntiVPNCommand extends Command
 	/** @var Manager **/
 	private Manager $manager;
 	
+	/** @var String **/
+	private String $wlUsage = "§8---====(§6Anti§cVPN§f WhiteList§8)====---\n§8>  §f/{command_label} {argument_label} add <player_name> §7To add whitelisted player.\n§8>  §f/{command_label} {argument_label} remove <player_name>§7 To remove whitelisted player.\n§8>  §f/{command_label} {argument_label} list §7To see the whitelist.";
+	
 	public function __construct() 
 	{
 		$this->manager = Manager::getInstance();
@@ -40,7 +47,7 @@ final class AntiVPNCommand extends Command
 		(
 			'antivpn',
 			'AntiVPN manager command',
-			'',
+			"§8---====(§6Anti§cVPN§8)====---\n§8>  §f/{command_label} setkey <key>§7 To set api key\n§8>  §f/{command_label} wl §7To manage AntiVPN whitelist.",
 			['avpn', 'antvpn', 'antiproxy']
 		);
 		$this->setPermission('antivpn.command');
@@ -85,9 +92,53 @@ final class AntiVPNCommand extends Command
 						}
 					break;
 					case 'wl':
+					case 'white-list':
+					case 'whitelist':
 						if ($this->testPermission($p, 'antivpn.command.whitelist'))
 						{
-							
+							if (isset($args[1]) && trim($args[1]) != '')
+							{
+								switch (strtolower($args[1]))
+								{
+									case 'add':
+									case 'set':
+									case 'new':
+										
+									break;
+									case 'remove':
+									case 'delete':
+									case 'unset':
+										if (isset($args[2]) && trim($args[2]) != '')
+										{
+											$user = $args[2];
+											if ($this->manager->isWhitelisted($user))
+											{
+												$user = strtolower($user);
+												$this->manager->getWhiteList()->remove($user);
+												$p->sendMessage("§7User {$args[2]} §7removed §a§lSuceffully§r§7.");
+											} else {
+												$p->sendMessage(self::COMMAND_PREFIX . "§7Player §f{$user} §7is not whitelisted!");
+											}
+										} else if ($p instanceof Player) {
+											(new RemovePlayerForm)->sendToPlayer($p);
+										} else {
+											$p->sendMessage(self::COMMAND_PREFIX . "§7To remove player use: §f/{$label} {$args[0]} {$args[1]} <player_name>");
+										}
+									break;
+									default:
+										if ($p instanceof Player) 
+										{
+											(new WhiteListMainForm())->sendToPlayer($p);
+										} else {
+											$this->showWhitelistUsageTo($p, $label, $args[0]);
+										}
+									break;
+								}
+							} else if ($p instanceof Player) {
+								(new WhiteListMainForm())->sendToPlayer($p);
+							} else {
+								$this->showWhitelistUsageTo($p, $label, $args[0]);
+							}
 						}
 					break;
 					default:
@@ -103,6 +154,17 @@ final class AntiVPNCommand extends Command
 	public function showUsageTo(CommandSender $p, String $label) : void 
 	{
 		$p->sendMessage(str_replace('{command_label}', $label, $this->getUsage()));
+	}
+	
+	public function showWhitelistUsageTo(CommandSender $player, String $commandLabel, String $whitelistLabel = 'wl') : void 
+	{
+		$usage = str_replace
+		(
+			['{command_label}', '{argument_label}'],
+			[$commandLabel, $whitelistLabel],
+			$this->wlUsage
+		);
+		$player->sendMessage($usage);
 	}
 	
 }
